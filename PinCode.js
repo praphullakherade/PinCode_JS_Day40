7b5e5440436be44a62a6c6292ebc93c0cb000626
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-24 17:31:54 +0530

    Pin code is a 6 digit code

--- PinCode.js
+++ PinCode.js
@@ -1,2 +1,19 @@
 
 console.log("Welcome to Pin Code Js Problem");
+
+class PinCode{
+    checkPatternNumber(data){
+        
+        var pattern=RegExp("^[0-9]{6}$");
+        
+        if(pattern.test(data)){
+            console.log("valid code");
+        }else{
+            console.log("invalid code");
+        }
+
+    }
+}
+
+const checkPattern=new PinCode();
+checkPattern.checkPatternNumber("431001");
